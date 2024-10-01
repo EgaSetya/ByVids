afd724ac4669d49c77945134e1668938b35cbdee
Ega Setya Putra <ega.putra@graveltechnology.com>
2024-10-01 11:51:26 +0700

    XCode project initial commit

---
+++ ByVids/ByVidsApp.swift
@@ -0,0 +1,17 @@
+//
+//  ByVidsApp.swift
+//  ByVids
+//
+//  Created by Ega Setya Putra on 01/10/24.
+//
+
+import SwiftUI
+
+@main
+struct ByVidsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
